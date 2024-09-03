26f10c6f2a392073ca571457b149f6dbb244e20f
PutraRoesad <putramuliaarhidanaroesad@mail.ugm.ac.id>
2024-09-03 16:10:42 +0700

    Add files via upload

---
+++ Assignment1/section1/add_numbers.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num1, num2, sum;
+    num1 = 2;
+    num2 = 3;
+    sum = num1 + num2;
+
+    cout << sum << endl;
+
+    return 0;
+}
